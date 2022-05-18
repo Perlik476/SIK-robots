8431855d88268fa414009fdc9cda656752541ca6
Perlik <mp429581@students.mimuw.edu.pl>
2022-05-18 11:30:53 +0200

    can't move out of board

--- src-client/definitions.h
+++ src-client/definitions.h
@@ -333,7 +333,7 @@ public:
 
     PlayerId my_id;
 
-    bool is_block_on_position(const Position& position) {
+    bool is_block_on_position(const Position &position) {
         for (const auto &block_position : blocks.list) {
             if (block_position->x.value == position.x.value
                 && block_position->y.value == position.y.value) {
@@ -357,22 +357,22 @@ public:
     void try_move(Direction direction, const std::shared_ptr<Position> &position) {
         switch(direction) {
             case Direction::Down:
-                if (!is_block_on_position(position->down())) {
+                if (!is_block_on_position(position->down()) && position->y.value > 0) {
                     position->y.value--;
                 }
                 break;
             case Direction::Left:
-                if (!is_block_on_position(position->left())) {
+                if (!is_block_on_position(position->left()) && position->x.value > 0) {
                     position->x.value--;
                 }
                 break;
             case Direction::Up:
-                if (!is_block_on_position(position->up())) {
+                if (!is_block_on_position(position->up()) && position->y.value < size_y.value - 1) {
                     position->y.value++;
                 }
                 break;
             case Direction::Right:
-                if (!is_block_on_position(position->right())) {
+                if (!is_block_on_position(position->right()) && position->x.value < size_x.value - 1) {
                     position->x.value++;
                 }
                 break;
